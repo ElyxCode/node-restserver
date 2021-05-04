28f8520b82703298256cc0819144e14df7ce41c0
Elyxcode <dfjuarezreyes21@gmail.com>
2021-05-04 15:59:12 -0600

    version base del servidor para servicios REST

--- server/server.js
+++ server/server.js
@@ -48,8 +48,6 @@ app.delete('/usuario', (req, res) => {
     res.json('deleteUsuario!');
 });
 
-
-
 app.listen(process.env.port, () => {
     console.log('Escuchando desde el puerto', process.env.PORT);
 });
