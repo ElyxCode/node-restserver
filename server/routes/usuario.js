33a9f205cfb043c47a08a7da3bb242d407088bb4
Elyxcode <dfjuarezreyes21@gmail.com>
2021-05-05 09:53:56 -0600

    Se agrego models usuario para DB y se validaron campos nombre, email y password con mensajes personalizados

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const app = express();
+const Usuario = require('../models/usuario.js');
 
 app.get('/usuario', (req, res) => {
 
@@ -11,17 +12,27 @@ app.post('/usuario', (req, res) => {
 
     let body = req.body;
 
-    if(body.nombre === undefined){
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre e necesario'
-        });
-    } else {
+    // Se formatea como se obtiene captura la información a partir del schema de usuario.
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: body.password,
+        role: body.role  
+    });
 
+    // Función que guarda el usuario en la base de datos
+    usuario.save((err, usuarioDB) => {
+        if(err) {
+            return res.status(400).json({
+               ok: false,
+               err
+            });
+        }
         res.json({
-            persona:  body
-        });
-    };
+            ok: true,
+            usuario: usuarioDB
+        })
+    });
 });
 
 app.put('/usuario/:id', (req, res) => {
