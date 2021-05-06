1c04b12f9c63697eac9c1d341b29720779123a4a
Elyxcode <dfjuarezreyes21@gmail.com>
2021-05-05 22:57:18 -0600

    Se configuro puertos en config/config.js de la base de datos para desplegar en la nube

--- server/config/config.js
+++ server/config/config.js
@@ -5,5 +5,27 @@
 
 process.env.PORT = process.env.PORT || 3000;
 
+// ======================
+// Entorno
+/// =====================
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ======================
+// Base detos
+// ======================
+
+let urlDatabase;
+
+if (process.env.NODE_ENV === 'dev') {
+
+    urlDatabase = 'mongodb://localhost:27017/cafe';
+   
+} else {
+
+    urlDatabase = 'mongodb+srv://douglas:echoes1992@sandbox.yojva.mongodb.net/cafe?retryWrites=true&w=majority';
+
+} 
+
+process.env.URLDB = urlDatabase;
 
